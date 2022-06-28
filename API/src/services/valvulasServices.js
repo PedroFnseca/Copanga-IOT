aaf0ef735198a8d3c938e227e8f9db8fa54ac2e7
Pedro Fonseca <97262778+pedroFnseca@users.noreply.github.com>
2022-06-28 12:03:02 -0300

    Arrumando service valvulas

--- API/src/services/valvulasServices.js
+++ API/src/services/valvulasServices.js
@@ -3,7 +3,7 @@ import database from '../database/connection.js'
 async function inserValvula(id_valvula, segundos){
     const conn = await database.connect()
 
-    const sql = 'INSERT INTO tbl_valvula(id_valvula, acionado, desligado) value (12, (select now() - interval 10 second), (select now()))'
+    const sql = 'INSERT INTO tbl_valvula(id_valvula, acionado, desligado) value (?, (select now() - interval ? second), (select now()))'
     
     const data = [id_valvula]
 
