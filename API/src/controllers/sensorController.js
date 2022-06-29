06f1bd98ea99d89a053f1f9e4dbdd041004b06b1
pedrofnseca <pedropereira.pp264@gmail.com>
2022-06-28 21:33:37 -0300

    validando dados dos sensores

--- API/src/controllers/sensorController.js
+++ API/src/controllers/sensorController.js
@@ -1,14 +1,24 @@
 import db from '../services/sensorServices.js'
 import express from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/', async (req, res) =>{
+router.post('/',[
+    body('idSensor').notEmpty().isNumeric().withMessage('idSensor invalido'),
+    body('valorSensor').notEmpty().isNumeric().withMessage('valorSensor inválido')
+], async (req, res) =>{
 
-    const {valorSensor, id_sensor} = req.body
+    const errosValidation = validationResult(req)
+
+    if(!errosValidation.isEmpty()){
+        return res.status(400).json({erro :errosValidation.array()})
+    }
+
+    const {valorSensor, idSensor} = req.body
 
     try{
-        await db.insertSensorValue(valorSensor, id_sensor)
+        await db.insertSensorValue(valorSensor, idSensor)
 
         res.status(201).json({
             msg: 'Registrado com exito'
