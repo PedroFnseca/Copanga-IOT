cc863078d785d213ed0142803a44f776dc68213c
Osmar Bruno <100640137+BRun0442@users.noreply.github.com>
2022-06-30 11:05:10 -0300

    Comentários no código

--- Esp32/PostHttpClient/EspHTTP_POST.ino
+++ Esp32/PostHttpClient/EspHTTP_POST.ino
@@ -11,11 +11,14 @@
 //#define SERVER_IP "10.0.1.7:9080" // PC address with emulation on host
 #define SERVER_IP "192.168.1.42"
 
+//Configuração de nome e senha da rede WiFi a ser conenctada
 #ifndef STASSID
 #define STASSID "your-ssid"
 #define STAPSK  "your-password"
 #endif
 
+
+//Variável global responsável por guardar pacote a ser enviado
 string jsonPayload = "{\"idValvula\":2,\"segundos\":15}";
 
 //==========================================================
@@ -25,18 +28,18 @@ void postHTTP(string endereco, string payload)
   HTTPClient http;
 
   Serial.print("[HTTP] begin...\n");
-    // configure traged server and url
+    //Inicia a comunicação http e envia o header na requisição
     http.begin(client, endereco); //HTTP
     http.addHeader("Content-Type", "application/json");
 
     Serial.print("[HTTP] POST...\n");
-    // start connection and send HTTP header and body
+    //httpCode é a variável que recebe o valor da requisição, se deu certo o valor é 200, responsável também por chamar a função que faz um POST na API.
     int httpCode = http.POST(payload);
 
-    // httpCode will be negative on error
+    //caso o código seja menor que 0, significa que o POST deu erro
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+      Serial.printf("Código HTTP: %d\n", httpCode);
 
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
@@ -46,7 +49,7 @@ void postHTTP(string endereco, string payload)
         Serial.println(">>");
       }
     } else {
-      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      Serial.printf("Ocorreu erro ao enviar a requisição POST, erro: %s\n", http.errorToString(httpCode).c_str());
     }
 
     http.end();  
