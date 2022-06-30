a51a97ad4c0cfe8e8e02d8e5e1ad25382515488d
Pedro Fonseca <97262778+pedroFnseca@users.noreply.github.com>
2022-06-30 09:51:54 -0300

    melhorando notFound

--- API/src/router.js
+++ API/src/router.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import sensor from './controllers/sensorController.js'
 import valvula from './controllers/valvulaController.js'
-import notFound from './controllers/notFound.js'
 
 const router = express.Router()
 
 router.use('/sensor', sensor)
 router.use('/valvula', valvula)
-router.use('/*', notFound)
+
+router.use('/*', (req, res) => {
+    res.status(404).json({message: 'Caminho não encontrado ou método.'});
+});
 
 
 export default router
