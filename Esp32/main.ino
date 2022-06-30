5bbb63a8643225b632d38997190672d226e41c08
Osmar Bruno <100640137+BRun0442@users.noreply.github.com>
2022-06-30 19:40:43 -0300

    Código main com comunicação HTTP

--- Esp32/main.ino
+++ Esp32/main.ino
@@ -1,6 +1,69 @@
+//Bibliotecas a serem utilizadas, biblioteca para conexão WiFi e para utilizar o protocolo HTTP
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+
+//Instruções caso a API esteja em localHost
+/* this can be run with an emulated server on host:
+        cd esp8266-core-root-dir
+        cd tests/host
+        make ../../libraries/ESP8266WebServer/examples/PostServer/PostServer
+        bin/PostServer/PostServer
+   then put your PC's IP address in SERVER_IP below, port 9080 (instead of default 80):
+*/
+#define SERVER_IP "10.0.1.7:9080" // endereço do PC, caso a API esteja em localhost
+#define SERVER_IP "192.168.1.42"
+
+//Configuração de nome e senha da rede WiFi a ser conenctada
+#ifndef ssid
+#define ssid "ssid"
+#define password  "password"
+#endif
+
+
+//Variável global responsável por guardar o pacote JSON a ser enviado
+string jsonPayload = "{\"idValvula\":2,\"segundos\":15}";
+
+
+//Funcionamento interno
 int Sensor1A, Sensor2A, Sensor1B, Sensor2B;
-int SensorSensivity;
-int measurementInterval;
+int SensorSensivity = 550;
+int measurementInterval = 300;
+
+
+void postHTTP(string endereco, string payload)
+{
+  WiFiClient client;
+  HTTPClient http;
+
+  Serial.print("Requisição iniciada...\n");
+    //Inicia a comunicação http e envia o header na requisição
+    http.begin(client, endereco); //HTTP
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.print("POST...\n");
+    //httpCode é a variável que recebe o valor da requisição, se deu certo o valor é 200, responsável também por chamar a função que faz um POST na API.
+    int httpCode = http.POST(payload);
+
+    //caso o código seja menor que 0, significa que o POST deu erro
+    if (httpCode > 0) {
+      // Mostra no monitor serial qual o código da requisição
+      Serial.printf("Código HTTP: %d\n", httpCode);
+
+      // Caso a requisição seja compreendida pela API, mostra no monitor serial o retorno da API
+      if (httpCode == HTTP_CODE_OK) {
+        const String& bodyGET = http.getString();
+        Serial.println("Pacote recebido:\n<<");
+        Serial.println(bodyGET);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("Ocorreu erro ao enviar a requisição POST, erro: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();  
+}
+
 
 void humidityMeasurement()
 {
@@ -30,6 +93,28 @@ void setup() {
   Serial.begin(9600);
 
   PORTD = B00000011;
+  
+  
+  //WiFi
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  //Inicia a conexão WiFi, passando o ssid e a senha
+  WiFi.begin(ssid, password);
+
+  //Enquanto não está conectado à rede, aparece pontinhos a cada meio segundo no monitor serial
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+        
+  //Após conectar, aparece o endereço IP
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -44,5 +129,14 @@ void loop() {
   if(millis() - millisData >= measurementInterval)
   {
     humidityMeasurement();
+  }
+  
+  //HTTP 
+  //Espera pela conexão WiFi, e a cada 10 segundos envia a requisição
+  if ((WiFi.status() == WL_CONNECTED)) {
+  
+  //Chama a função para enviar o endereço da API e o pacote JSON
+  postHTTP("http://" SERVER_IP "/postplain/", jsonPayload);
+  delay(10000);
   }
 }
