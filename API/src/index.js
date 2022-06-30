bddd66945b5217c096fc6b6b0a8a3a3ed70c29aa
pedrofnseca <pedropereira.pp264@gmail.com>
2022-06-30 16:13:08 -0300

    API no ar

--- API/src/index.js
+++ API/src/index.js
@@ -3,9 +3,10 @@ import routes from './router.js'
 
 const api = express()
 api.use(express.json())
+const port = 3333
 
 api.use('/', routes)
 
-api.listen('3333', () =>{
-    console.log('server is running')
+api.listen(process.env.PORT || port, () =>{
+    console.log(`server is running in ${port}`)
 })
