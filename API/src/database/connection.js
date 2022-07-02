fe489e6040ecf25912ddce087ced89a7ab3fe316
pedrofnseca <pedropereira.pp264@gmail.com>
2022-07-02 18:25:33 -0300

    tirando credenciais

--- API/src/database/connection.js
+++ API/src/database/connection.js
@@ -2,15 +2,13 @@ import mysql from 'mysql2/promise'
 
 async function connect(){
     const connection = await mysql.createConnection({
-        host: 'bzzqpyjvvn4vklv2rj2l-mysql.services.clever-cloud.com',
-        user: 'ulzltm6wbb6psoj4',
-        password: 'CFB7CFtonE7z0HNuLxRk',
-        database: 'bzzqpyjvvn4vklv2rj2l'
+        host: 'hostname',
+        user: 'root',
+        password: '',
+        database: 'bd_copanga'
     })   
 
     return connection
 }
 
-
 export default {connect}
- 
