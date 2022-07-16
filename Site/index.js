4d0a768bb116efd97d623347ad25a34719c9d3c6
pedrofnseca <pedropereira.pp264@gmail.com>
2022-07-15 23:00:03 -0300

    Gráfico dinamico funcionando

--- Site/index.js
+++ Site/index.js
@@ -1,43 +1,74 @@
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-
-const data = {
-    labels: labels,
-    datasets: [{
-      label: 'Sensor 7',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [60, 10, 5, 2, 20, 30, 45, 7],
-    },
-    {
-        label: 'Sensor 2',
-        backgroundColor: 'rgb(50, 99, 132)',
-        borderColor: 'rgb(50, 99, 132)',
-        data: [54, 5, 15, 12, 2, 3, 40, 7],
-      }
-],
-};
-
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
-
-const myChart = new Chart(
-    document.getElementsByClassName('myChart'),
-    config
-);
-
 const url = 'https://api-irrigacao.herokuapp.com/sensor/allData'
 
-fetch(url)
-    .then((response) =>{
-        console.log(JSON.parse(response))
+async function getData(url, count){
+    const data = await axios.get(url)
+
+    return data.data.results.slice(0, count)
+}
+
+function collectValue(dataJson){
+    let dados = []
+    dataJson.map((dataSensor) =>{
+        return dados.push(dataSensor.valorSensor)
     })
+
+    return dados
+}
+
+function formatdate(date, format){
+    const YMD = date.split('T')
+    const separatedDate = YMD[0].split('-')
+    const separatedHour = YMD[1].split(':')
+
+    var hourString = `${separatedHour[0]}:${separatedHour[1]}`
+    var dateString = `${separatedDate[2]}-${separatedDate[1]}`
+
+    if(format){
+        return `${hourString} ${dateString}`
+    } 
+    else {
+        return `${hourString}`
+    }
+}
+
+function collectDate(dataJson){
+    let dados = []
+    dataJson.map((dataSensor) =>{
+
+        const date = formatdate(dataSensor.dataHora, false)
+
+        return dados.push(date)
+    })
+
+    return dados
+}
+
+getData(url, 25)
+.then(response =>{
+    console.log(response)
+    
+    const dados = collectValue(response)
+
+    const labels = collectDate(response)
+    
+    const data = {
+        labels: labels,
+        datasets: [{
+          label: 'Sensor 7',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: dados,
+        },
+    ],
+    };
+    
+    const config = {
+        type: 'line',
+        data: data
+    };
+    
+    const myChart = new Chart(
+        document.getElementsByClassName('myChart'),
+        config
+    );
+})
