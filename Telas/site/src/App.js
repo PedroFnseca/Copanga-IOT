e0585c56f73d15677ec2d6066800a70275d8d7f8
pedrofnseca <pedropereira.pp264@gmail.com>
2022-07-23 15:51:03 -0300

    add axios

--- Telas/site/src/App.js
+++ Telas/site/src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Canvas from './Components/Canvas/index';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Canvas />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
