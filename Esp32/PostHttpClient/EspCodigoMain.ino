16e601a33202438bbb1310fa2438001ad91ffc7a
Osmar Bruno <100640137+BRun0442@users.noreply.github.com>
2022-08-06 14:11:15 -0300

    Código comentado

--- Esp32/PostHttpClient/EspCodigoMain.ino
+++ Esp32/PostHttpClient/EspCodigoMain.ino
@@ -1,3 +1,5 @@
+CodigoPrincipal
+
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
@@ -12,8 +14,8 @@ int valorSensor[2];
 int valvula[2];
 int valvulaTimer[] = 0;
 
-int measurementInterval = 10;
-int SensorSensivity = 55;
+int intervalo = 10;
+int sensibilidadeSensor = 55;
 int millisData = 0;
 
 //Senha da API
@@ -24,77 +26,92 @@ const String SensorAPI = "http://api-irrigacao.herokuapp.com/sensor";
 const String ValvulaAPI = "http://api-irrigacao.herokuapp.com/valvula";
 
 
-//Void que envia as requisições POST
+//Função que envia as requisições POST
 void postHTTP(String endereco, String payload)
 {
-  WiFiClient client;
-  HTTPClient http;
+    //httpCode é a variável que recebe o valor da requisição, se deu certo o valor é 200, responsável também por chamar a função que faz um POST na API.
+    int httpCode = http.POST(payload);
 
-  Serial.print("Requisição iniciada...\n");
-  //Inicia a comunicação http e envia o header na requisição
-  http.begin(client, endereco); //HTTP
-  http.addHeader("Content-Type", "application/json");
+    //Cliente que está acessando, sendo este o próprio esp e a criação de uma instância de um cliente http
+    WiFiClient client;
+    HTTPClient http;
 
-  Serial.print("POST...\n");
-  //httpCode é a variável que recebe o valor da requisição, se deu certo o valor é 200, responsável também por chamar a função que faz um POST na API.
-  int httpCode = http.POST(payload);
 
-  //caso o código seja maior que 0 ou seja, retornou o código
-  if (httpCode > 0) {
-    // Mostra no monitor serial qual o código da requisição
-    Serial.printf("Código HTTP: %d\n", httpCode);
+    //Inicia a comunicação http e envia o header na requisição
+    Serial.print("Requisição iniciada...\n");
+    http.begin(client, endereco); //HTTP
+    http.addHeader("Content-Type", "application/json");
+    Serial.print("POST...\n");
+    
 
-    // Caso a requisição seja compreendida pela API, mostra no monitor serial o body da requisição
-    if (httpCode == HTTP_CODE_OK) {
-      const String& bodyGET = http.getString();
-      Serial.println("Pacote recebido:\n<<");
-      Serial.println(bodyGET);
-      Serial.println(">>");
+    //caso o código seja maior que 0 ele retornou um código do corpo da requisição
+    if (httpCode > 0) {
+        // Mostra no monitor serial qual o código da requisição
+        Serial.printf("Código HTTP: %d\n", httpCode);
+
+        // Caso a requisição seja compreendida pela API, mostra no monitor serial o body da requisição
+        if (httpCode == HTTP_CODE_OK) {
+            //bodyGET recebe o que há escrito no corpo da pagina
+            const String& bodyGET = http.getString();
+            Serial.println("Pacote recebido:\n<<");
+            Serial.println(bodyGET);
+            Serial.println(">>");
     }
     } else {
-      Serial.printf("Ocorreu erro ao enviar a requisição POST, erro: %s\n", http.errorToString(httpCode).c_str());
+        //Mostra o código de erro
+        Serial.printf("Ocorreu erro ao enviar a requisição POST, erro: %s\n", http.errorToString(httpCode).c_str());
     }
+
+    //Finaliza a requisição
     http.end();  
 }
 
 
-//função responsavel por receber os dados dos sensores ou valvulas e retornar o json a ser enviado
+//função responsavel por receber os dados dos sensores ou valvulas e retornar o json
 String json(String caminho, int id, int value)
 {
-  String jsonPayload;
-  //verifica se o json é para o sensor ou para as valvulas
-  switch(caminho)
-  {
-    case "sensor":
-      jsonPayload = "{\"key\":"apiKey",\"idSensor\":" + String(id) + ",\"valorSensor\":" + String(value) + "}";
-    break;
-    
-    case "valvula":
-      jsonPayload = "{\"key\":"apiKey",\"idValvula\":" + String(id) + ",\"segundos\":" + String(value) + "}";
-    break;
-  }
+    String jsonPayload;
+
+    //verifica se o json é para o sensor ou para as valvulas
+    switch(caminho)
+    {
+        case "sensor":
+            jsonPayload = "{\"key\":"apiKey",\"idSensor\":" + String(id) + ",\"valorSensor\":" + String(value) + "}";
+        break;
+
+        case "valvula":
+            jsonPayload = "{\"key\":"apiKey",\"idValvula\":" + String(id) + ",\"segundos\":" + String(value) + "}";
+        break;
+    }
   
+  //Retorna a variavel com o json formatado e pronto para ser enviado para a api
   return jsonPayload;
 }
 
+//Função responsável por gerir as válvulas e o tempo em que elas estão ligadas
 void valvula(int pin, int id, bool onOff)
 {
-  //Caso queira ligar a válvula ela verifica se o indice do vetor é zero ou nulo
-  //se for, ele registra o tempo inicial
+  //Caso queira ligar a válvula ela verifica se o indice do vetor é zero ou nulo, para evitar que ela
+  //sobrescreva sobre o valor inicial, se for, ele registra o tempo inicial
   if(onOff == true && (valvulaTimer[id] == 0 || valvulaTimer[id] == null))
   {
+    //Acionamento da valvula
     digitalWrite(pin, HIGH);
-    //contagem de tempo aqui
+    //A valvula recebe o valor millis quando é acionada, tendo assim o tempo de inicio
     valvulaTimer[id] = millis();
-  }else if(onOff == false)
+  }
+  //Caso a valvula estiver sendo desligada
+  else if(onOff == false)
   {
-    //Se eu desligar a valvula ele retorna o tempo corrido desde o acionamento da válvula
-    //que é igual ao meu tempo atual menos o tempo em que ela estava ligada
+    //Se a valvula for desligada a função retorna o tempo corrido desde o acionamento da válvula
+    //que é igual ao meu tempo atual menos o tempo em que ela esteve ligada
     digitalWrite(pin, LOW);
     return (millis() - valvulaTimer[id]);
   }
 }
 
+//Void que lê o valor dos sensores externos, tendo em vista que o valor recebido vai de 0 a 4095 
+//é aplicado uma regra de tres para transformar em porcentagem
 void humidityMeasurement()
 {
   valorSensor[0] = map(analogRead(35), 0, 4095, 100, 0);
@@ -103,86 +120,91 @@ void humidityMeasurement()
 
 
 void setup() {  
-  Serial.begin(115200);
-
-  Serial.println();
-
-  //inicia a conexão WiFi
-  WiFi.begin(STASSID, STAPSK);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.begin(115200);
+    Serial.println();
 
 
-  pinValvula[0]
+    //inicia a conexão WiFi, passando o nome e a senha da rede WiFi
+    WiFi.begin(STASSID, STAPSK);
+
+    //Enquanto não estiver conectado o código não procede para as próximas etapas
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    //Conectado, aparece o IP da rede
+    Serial.println("");
+    Serial.print("Connected! IP address: ");
+    Serial.println(WiFi.localIP());
 
 
-  //OTA
+    //OTA
 
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
+    // Port defaults to 3232
+    // ArduinoOTA.setPort(3232);
 
-  // Hostname defaults to esp3232-[MAC]
-  ArduinoOTA.setHostname("esp32");
+    // Hostname defaults to esp3232-[MAC]
+    ArduinoOTA.setHostname("esp32");
 
-  // No authentication by default
-  ArduinoOTA.setPassword("admin");
+    // No authentication by default
+    ArduinoOTA.setPassword("admin");
 
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA
+    ArduinoOTA
     .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
         type = "sketch";
-      else // U_SPIFFS
+        else // U_SPIFFS
         type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
     })
     .onEnd([]() {
-      Serial.println("\nEnd");
+        Serial.println("\nEnd");
     })
     .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
     })
     .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
     });
 
-  ArduinoOTA.begin();
+    ArduinoOTA.begin();
 }
 
 void loop() {
-  ArduinoOTA.handle();
-  
-  
-  //Espera pela conexão WiFi
-  if ((WiFi.status() == WL_CONNECTED)) {
-    if(millis() - millisData >= (measurementInterval * 60000))
-    {
-      millisData = millis();
-      humidityMeasurement();
-      
-      //comando para enviar requisição para o sensor,o primeiro parametro é o endereço, 
-      //e o segundo é um objeto String que retorna formatado o json
-      postHTTP(SensorAPI, json("sensor", 0, valorSensor[0]));
-      postHTTP(SensorAPI, json("sensor", 1, valorSensor[1]));
-      postHTTP(ValvulaAPI, json("valvula", 0, valvula(24, 0, HIGH)));
-      postHTTP(ValvulaAPI, json("valvula", 1, valvula(25, 1, HIGH)));
+    ArduinoOTA.handle();
+
+
+    //Espera pela conexão WiFi
+    if ((WiFi.status() == WL_CONNECTED)) {
+        //Condicional que é executada de tempos em tempos tendo como intervalo a variavel supra comentada
+        if(millis() - millisData >= (intervalo * 60000))
+        {
+            //A variavel auxiliar de tempo é sobrescrita e passa a ser o tempo atual,
+            //refazendo assim o ciclo.
+            millisData = millis();
+
+            //Medição dos sensores
+            humidityMeasurement();
+            
+            //comando para enviar requisição para o sensor,o primeiro parametro é o endereço, 
+            //e o segundo é um objeto String que retorna formatado o json
+            postHTTP(SensorAPI, json("sensor", 0, valorSensor[0]));
+            postHTTP(SensorAPI, json("sensor", 1, valorSensor[1]));
+            postHTTP(ValvulaAPI, json("valvula", 0, valvula(24, 0, HIGH)));
+            postHTTP(ValvulaAPI, json("valvula", 1, valvula(25, 1, HIGH)));
+        }
     }
-  }
 }
