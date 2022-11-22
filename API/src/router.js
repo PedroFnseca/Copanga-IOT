90977ce9b672b5b8389631226cb0daab7ecaf4e6
Pedro Fonseca <97262778+PedroFnseca@users.noreply.github.com>
2022-11-22 12:21:19 -0300

    :sparkles: feat: base de rota meteorologia

--- API/src/router.js
+++ API/src/router.js
@@ -1,14 +1,16 @@
-import express from 'express'
-import sensor from './controllers/sensorController.js'
-import valvula from './controllers/valvulaController.js'
+import express from "express";
+import sensor from "./controllers/sensorController.js";
+import valvula from "./controllers/valvulaController.js";
+import meteorologia from "./controllers/meteorologiaController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/sensor', sensor)
-router.use('/valvula', valvula)
+router.use("/sensor", sensor);
+router.use("/valvula", valvula);
+router.use("/meteorologia", meteorologia);
 
-router.use('/*', (req, res) => {
-    res.status(404).json({message: 'Caminho ou método não encontrado.'});
+router.use("/*", (req, res) => {
+  res.status(404).json({ message: "Caminho ou método não encontrado." });
 });
 
-export default router
+export default router;
