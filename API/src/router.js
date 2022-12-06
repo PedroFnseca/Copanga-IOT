19dba38bf59aba387263e51cb666f049c0adc648
pedrofnseca <pedropereira.pp264@gmail.com>
2022-12-06 15:53:52 -0300

    feat rota de emprestimo

--- API/src/router.js
+++ API/src/router.js
@@ -2,12 +2,14 @@ import express from "express";
 import sensor from "./controllers/sensorController.js";
 import valvula from "./controllers/valvulaController.js";
 import meteorologia from "./controllers/meteorologiaController.js";
+import report from "./controllers/reportController.js";
 
 const router = express.Router();
 
 router.use("/sensor", sensor);
 router.use("/valvula", valvula);
 router.use("/meteorologia", meteorologia);
+router.use("/report", report)
 
 router.use("/*", (req, res) => {
   res.status(404).json({ message: "Caminho ou método não encontrado." });
